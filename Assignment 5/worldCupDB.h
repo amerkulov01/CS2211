85708c31383cc0398172d47f2e634382f318e23c
Andrei Merkulov <119703483+amerkulov01@users.noreply.github.com>
2022-12-28 14:41:25 -0500

    Create worldCupDB.h

---
+++ Assignment 5/worldCupDB.h
@@ -0,0 +1,12 @@
+/*
+Title: CS2211 Assignment 5
+Purpose: Header file for worldCupDB.c
+Author: Andrei Merkulov
+Student Number: 251145994
+*/
+
+#include <stdio.h>
+#include "worldcup_team.h"
+#include "worldcup_player.h"
+
+extern void print_help(void);
